74b497155660f378e889677492d63f238a0b2289
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-08-21 18:19:47 +0700

    feat: export Tao decision primitives

--- src/bardocompute/__init__.py
+++ src/bardocompute/__init__.py
@@ -1,4 +1,13 @@
 from .line import BardoLine, LineState, TransitionMode
+from .tao import DecisionEvidence, TaoDecision, decide_tao
 from .trigram import BardoTrigram
 
-__all__ = ["BardoLine", "LineState", "TransitionMode", "BardoTrigram"]
+__all__ = [
+    "BardoLine",
+    "LineState",
+    "TransitionMode",
+    "BardoTrigram",
+    "DecisionEvidence",
+    "TaoDecision",
+    "decide_tao",
+]
